13948b54e11f19c591a285d7999180d04f3e636d
Catherine Salazar <catcreates13@gmail.com>
2019-01-26 03:19:09 +0000

    Automatically backed up by Learn

---
+++ models/piglatinizer.rb
@@ -0,0 +1,29 @@
+require 'pry'
+
+class PigLatinizer
+
+  def piglatinize(word)
+    vowels = %w|A E I O U a e i o u|
+    consonants = ""
+    #binding.pry
+    if vowels.include?(word[0])
+       "#{word}way"
+     else
+       while !vowels.include?(word[0])
+         consonants << word[0]
+         word = word.split("")[1..-1].join
+       end
+       "#{word}#{consonants}ay"
+     end
+   end
+
+
+
+  def to_pig_latin(each_word)
+    each_word.split.map { |word| piglatinize(word)}.join(" ")
+  end
+
+end
+
+#consonant sounds, move consonant/s to the end, add put "ay"
+#vowel sounds,just adds "way" or "yay" to the end (or just "ay").
