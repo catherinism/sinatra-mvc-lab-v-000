7f848fd28d98329c4dedc36060284093de8c18b5
Catherine Salazar <catcreates13@gmail.com>
2019-01-26 03:32:37 +0000

    Automatically backed up by Learn

--- models/piglatinizer.rb
+++ models/piglatinizer.rb
@@ -7,17 +7,17 @@ class PigLatinizer
     vowels = %w|A E I O U a e i o u|
     consonants = ""
     #binding.pry
-    if !vowels.include?(word[0])
-      consonants << word[0]
-      word = word.split("")[1..-1].join
-    end
-      "#{word}#{consonants}ay"
-      else
-        while vowels.include?(word[0])
+    if vowels.include?(word[0])
            "#{word}way"
-         
+         else
+           while !vowels.include?(word[0])
+             consonants << word[0]
+             word = word.split("")[1..-1].join
+           end
+           "#{word}#{consonants}ay"
+         end
        end
-     end
+
 
   end
 
